807a225599dda2cd8032823854866106d42ad17b
iChwn <ichwanarif26@gmail.com>
2019-09-10 10:22:53 +0700

    Add Libs & Configure :
    --Add Libs--
    - Redux
    - Tailwind css
    - React router
    
    --Configure Tailwind Css--

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="text-center">
+      <header className="bg-blue-900 m-6 p-6 rounded shadow-lg flex flex-col items-center">
+        <img src={logo} className="App-logo w-32 h-32" alt="logo" />
+        <h1 className="text-white text-3xl">Welcome to React</h1>
       </header>
+      <p className="text-base">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
